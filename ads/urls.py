82b3532bf1816c46832213b6f29cfc5e66451abf
greenpearbear <romadmitr12@gmail.com>
2022-05-09 01:04:41 +0700

    Доделаны детальные запросы по pk

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cat/', views.CategoriesView.as_view(), name='POST_and_GET_categories'),
+    path('ad/', views.AnnouncementsView.as_view(), name='POST_and_GET_announcement'),
+    path('cat/<int:pk>/', views.CategoriesViewDetail.as_view(), name='detail_cat'),
+    path('ad/<int:pk>/', views.AnnouncementsViewDetail.as_view(), name='detail_ad')
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,8 +1,11 @@
 import json
 
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import ListView
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
+
 from ads.models import Categories, Announcement
 
 
@@ -10,44 +13,7 @@ def index(request):
     return JsonResponse({"status": "ok"}, status=200)
 
 
-class CategoriesViewList(ListView):
-
-    model = Categories
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-
-        response = []
-
-        for categories in self.object_list:
-            response.append({
-                'id': categories.pk,
-                "name": categories.name,
-            })
-
-        return JsonResponse(response, safe=False)
-
-
-class AnnouncementViewList(ListView):
-
-    model = Announcement
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-
-        response = []
-
-        for announcement in self.object_list:
-            response.append({
-                'id': announcement.pk,
-                "name": announcement.name,
-                'author': announcement.author,
-                'price': announcement.price
-            })
-
-        return JsonResponse(response, safe=False)
-
-
+@method_decorator(csrf_exempt, name='dispatch')
 class AnnouncementsView(View):
     def post(self, request):
 
@@ -74,7 +40,24 @@ class AnnouncementsView(View):
 
         })
 
+    def get(self, request):
 
+        announcements = Announcement.objects.all()
+
+        response = []
+
+        for announcement in announcements:
+            response.append({
+                'id': announcement.pk,
+                "name": announcement.name,
+                'author': announcement.author,
+                'price': announcement.price
+            })
+
+        return JsonResponse(response, safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
 class CategoriesView(View):
     def post(self, request):
 
@@ -83,6 +66,55 @@ class CategoriesView(View):
         categories = Categories()
 
         categories.name = categories_data['name']
+
+        categories.save()
+
+        return JsonResponse({
+            'id': categories.pk,
+            'name': categories.name
+        })
+
+    def get(self, request):
+
+        categories = Categories.objects.all()
+
+        response = []
+
+        for categories in categories:
+            response.append({
+                'id': categories.pk,
+                "name": categories.name,
+            })
+
+        return JsonResponse(response, safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AnnouncementsViewDetail(DetailView):
+
+    model = Announcement
+
+    def get(self, request, *args, **kwargs):
+        announcement = self.get_object()
+
+        return JsonResponse({
+                'id': announcement.pk,
+                "name": announcement.name,
+                'author': announcement.author,
+                'price': announcement.price,
+                'description': announcement.description,
+                'address': announcement.address,
+                'is_published': announcement.is_published
+        })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoriesViewDetail(DetailView):
+
+    model = Categories
+
+    def get(self, request, *args, **kwargs):
+        categories = self.get_object()
 
         return JsonResponse({
             'id': categories.pk,
