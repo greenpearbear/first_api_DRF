261ca8c6596353a1b93631d33626406386622c80
greenpearbear <romadmitr12@gmail.com>
2022-05-17 00:04:04 +0700

    Правил фикстуры

--- datasets/create_fixtures.py
+++ datasets/create_fixtures.py
@@ -11,10 +11,13 @@ def fixtures_create(json_file_path):
 
     for json_data in json_array:
         json_array_new.append({
-            "id": json_data["id"],
-            "name": json_data["name"],
-            "lat": json_data["lat"],
-            "lng": json_data["lng"],
+            "first_name": json_data["first_name"],
+            "last_name": json_data["last_name"],
+            "username": json_data["username"],
+            "password": json_data["password"],
+            "role": json_data["role"],
+            "age": json_data["age"],
+            "location_id": json_data["location_id"],
         })
 
     json_fixture = []
@@ -22,13 +25,14 @@ def fixtures_create(json_file_path):
 
     for json_data in json_array_new:
         json_fixture.append({
-            "model": "user.location",
+            "model": "user.author",
             "pk": i,
             "fields": json_data
         })
+        i += 1
 
-    with open('location_fixture.json', 'w', encoding='utf-8') as jsonf:
+    with open('../user_fixture.json', 'w', encoding='utf-8') as jsonf:
         json.dump(json_fixture, jsonf, indent=4)
 
 
-fixtures_create(r'LOCATION.json')
+fixtures_create(r'USER.json')
